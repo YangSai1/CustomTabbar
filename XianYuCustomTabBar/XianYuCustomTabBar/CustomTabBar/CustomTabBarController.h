5160b4290e161e1d908c7986708b19c5c870d435
Mac <475747592@qq.com>
2017-07-17 10:11:47 +0800

    初始化了新的工程

---
+++ XianYuCustomTabBar/XianYuCustomTabBar/CustomTabBar/CustomTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarController.h
+//  XianYuCustomTabBar
+//
+//  Created by jiang on 2017/3/9.
+//  Copyright © 2017年 skydrui.regular. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CustomTabBar.h"
+
+@interface CustomTabBarController : UITabBarController
+
+@property(nonatomic, strong)CustomTabBar *mainTabBar;
+
+
+@end
---
+++ XianYuCustomTabBar/XianYuCustomTabBar/HomeViewController.m
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.m
+//  XianYuCustomTabBar
+//
+//  Created by jiang on 2017/3/9.
+//  Copyright © 2017年 skydrui.regular. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "WZLBadgeImport.h"
+#import "CustomTabBarController.h"
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    CustomTabBarController *tabbarVc = (CustomTabBarController *)self.tabBarController;
+    [tabbarVc.mainTabBar showBageView:@"1" index:0];
+    
+    // Do any additional setup after loading the view.
+//    self.view.backgroundColor = [UIColor lightGrayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
